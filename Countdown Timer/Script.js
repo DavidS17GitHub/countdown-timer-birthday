44ceb1a0a23321941d76d9bf569da6b13a0dc1a8
DavidS17GitHub <108815743+DavidS17GitHub@users.noreply.github.com>
2022-07-13 17:01:44 -0500

    Add files via upload

---
+++ Countdown Timer/Script.js
@@ -0,0 +1,40 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+
+const nextBirthday = '17 April 2023'
+
+function countdown() {
+    const nextBirthdayDate = new Date(nextBirthday);
+    const currentDate = new Date();
+
+    const totalSeconds = (nextBirthdayDate-currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+
+    const hours = Math.floor(totalSeconds / 3600) %24;
+
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+
+    const seconds = Math.floor(totalSeconds % 60);
+
+    console.log(days, hours, minutes, seconds);
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minutesEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
+
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+//initial call
+
+countdown();
+
+setInterval(countdown, 1000);
